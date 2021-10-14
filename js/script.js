a81626f91af1855531ab34a1a13a9725ccf53c24
Jorge Martinez <tnlcomputer@gmail.com>
2021-10-14 15:04:38 -0300

    mi primer commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* All HTMLElements have a "$" at beginning*/
+const $shareButton = document.getElementById("shareButton"),
+  $footerMenu = document.getElementById("cardMenu");
+
+document.body.addEventListener(
+  "click",
+  (e) => {
+    if (!e.target.classList.contains("card__menu--hidden"))
+      $footerMenu.classList.add("card__menu--hidden");
+  },
+  true
+);
+
+$shareButton.addEventListener("click", () =>
+  $footerMenu.classList.toggle("card__menu--hidden")
+);
